79d56bed030a7f4c04666f9ffd9e5ef0bad3ef53
joao9vegeto-stack <joao9vegeto@gmail.com>
2026-05-25 22:24:52 -0300

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -26,7 +26,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 const downloadsDir = path.join(__dirname,"downloads");
 
